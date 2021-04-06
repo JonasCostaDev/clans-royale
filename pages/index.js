9d09524aa67680a9a9058284b452cd6e3d420c47
Cristiano Cruz <cristiano_nsc@hotmail.com>
2021-04-06 20:27:03 -0300

    feat: create api integration

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import axios from 'axios';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ clans }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,55 +12,38 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Clash Royale Clans!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <ul>
+                {clans?.items?.map((item) => (
+                    <li>{item.name} - {item.type} - {item.members}</li>
+                ))}
+            </ul>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>Depois nos ajustaremos...</p>
       </footer>
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const myApiKey = 'YOUR_API_KEY'
+  const api = axios.create({
+    baseURL: 'https://api.clashroyale.com/v1/',
+    headers: {
+      authorization: `Bearer ${myApiKey}`
+    }
+  })
+  const response = await api.get('/clans?name=Brasil%20Squad');
+  return {
+    props: {
+      clans: response.data,
+    },
+  }
+}
+
